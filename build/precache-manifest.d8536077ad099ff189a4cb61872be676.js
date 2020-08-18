48cf3a44de9f83c786d629dacb39ff179c62b74e
ahmaadidrees <ahmaadidrees@gmail.com>
2020-08-17 17:17:46 -0700

    poop

---
+++ build/precache-manifest.d8536077ad099ff189a4cb61872be676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a6f4ae9ad3c6c235c105bbd8095af7",
+    "url": "/react-true-split/index.html"
+  },
+  {
+    "revision": "2b6b67b4911e8a11f431",
+    "url": "/react-true-split/static/css/main.de26ba21.chunk.css"
+  },
+  {
+    "revision": "6234b6a6e711f0322cbb",
+    "url": "/react-true-split/static/js/2.1f1643a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-true-split/static/js/2.1f1643a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6b67b4911e8a11f431",
+    "url": "/react-true-split/static/js/main.b71a6835.chunk.js"
+  },
+  {
+    "revision": "e82aeef4e1ca79adfe54",
+    "url": "/react-true-split/static/js/runtime-main.6fb7e756.js"
+  }
+]);
